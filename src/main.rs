f917329b508b052cfca683923fea989712875ff3
Luke <lddtwo45@gmail.com>
2024-08-10 01:36:53 -0400

    finally made a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use glium::{winit};
+
 fn main() {
-    println!("Hello, world!");
+    // handles events like mouse clicks
+    let event_loop = glium::winit::event_loop::EventLoopBuilder::new().build().expect("event loop building");
+    // window and display for drawing frames 
+    let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
+
+    // start loop
+    let _ = event_loop.run(move |event, window_target| {
+        match event {
+            glium::winit::event::Event::WindowEvent { event, .. } => match event {
+                glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
+                _ => (),
+            },
+            _ => (),
+        };
+    });
 }
