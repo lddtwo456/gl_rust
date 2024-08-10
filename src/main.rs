aa74aeb38bbe291e8e0298c6f91be0a04fd1117a
Luke <lddtwo45@gmail.com>
2024-08-10 01:53:39 -0400

    get those annoying yellow squiggles outta here

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use glium::{winit};
+use glium::{winit, backend};
 
+#[allow(deprecated)]
 fn main() {
     // handles events like mouse clicks
-    let event_loop = glium::winit::event_loop::EventLoopBuilder::new().build().expect("event loop building");
+    let event_loop = winit::event_loop::EventLoopBuilder::new().build().expect("event loop building");
     // window and display for drawing frames 
-    let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
+    let (_window, display) = backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
 
     // start loop
     let _ = event_loop.run(move |event, window_target| {
         match event {
-            glium::winit::event::Event::WindowEvent { event, .. } => match event {
-                glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
+            winit::event::Event::WindowEvent { event, .. } => match event {
+                winit::event::WindowEvent::CloseRequested => window_target.exit(),
                 _ => (),
             },
             _ => (),
